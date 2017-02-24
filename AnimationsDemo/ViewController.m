821ab58e2616f3f0b7f529f9dd5688d27c027700
leihui <leihui@felink.com>
2017-02-24 18:45:38 +0800

    First Commit

---
+++ AnimationsDemo/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  AnimationsDemo
+//
+//  Created by leihui on 17/2/24.
+//  Copyright © 2017年 ND WebSoft Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+	
+	self.view.backgroundColor = [UIColor orangeColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
